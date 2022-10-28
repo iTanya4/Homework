536f6d9303134609f351677dce0e971966671bb5
TatYana <4inenova@gmail.com>
2022-10-28 16:21:54 +0300

    добавила работу Алины

---
+++ Sem1/Program.cs
@@ -0,0 +1,62 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//Задача №1. Напишите программу, которая на вход принимает два числа и проверяет, 
+//является ли первое число квадратом второго.
+//a = 25, b = 5 -> да 
+//a = 2, b = 10 -> нет 
+//a = 9, b = -3 -> да 
+//a = -3 b = 9 -> нет
+
+
+// Console.WriteLine("Введите натуральное число ");
+// int x=Convert.ToInt32 ( Console.ReadLine());
+// Console.WriteLine("Введите натуральное число ");
+// int y=Convert.ToInt32 ( Console.ReadLine());
+// if (x*x == y)  Console.WriteLine("Второе число является квадратом первого");
+// else if (y*y == x) Console.WriteLine("Первое число является квадратом второго");
+// else Console.WriteLine("No");
+
+//Задача №5. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
+// 2 -> " -2, -1, 0, 1, 2"
+// Console.WriteLine("Введите натуральное число ");
+// int x=Convert.ToInt32 ( Console.ReadLine());
+// int y=-x;
+// while (y<x)
+// {
+//    // Console.Write($"{y}, ");
+//     Console.Write(y + ", ");
+
+//     y=y+1;
+// }
+// Console.WriteLine(y);
+
+// Задача №7. Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает последнюю цифру этого числа.
+// 	456 -> 6
+// 	782 -> 2
+// 	918 -> 8
+
+
+// как найти второе с конца число c#
+// Console.WriteLine("Введите натуральное число ");
+// int x=Convert.ToInt32 ( Console.ReadLine());
+// int y = x % 10;
+// Console.WriteLine(y);
+ 
+
+// Задача №3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+// 3 -> Среда 
+// 5 -> Пятниц
+ //Console.WriteLine("Введите число дня недели");
+ //int x=Convert.ToInt32 ( Console.ReadLine());
+
+ //if (x==1) Console.WriteLine("Понедельник");
+ //else if (x==2) Console.WriteLine("Вторник");
+ //else if (x==3) Console.WriteLine("Среда");
+//else if (x==4) Console.WriteLine("Четверг");
+//else if (x==5) Console.WriteLine("Пятница");
+//else if (x==6) Console.WriteLine("Суббота");
+// else if (x==7) Console.WriteLine("Воскесенье");
+ //else Console.WriteLine("Вы ввели не то число");
