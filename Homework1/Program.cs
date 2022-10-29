5fa82ba3d6ae70f2d769507a53217f760a251d59
TatYana <4inenova@gmail.com>
2022-10-29 14:59:10 +0300

    Добавили все файлы

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -3,9 +3,9 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 Console.WriteLine("Введите первое число");
-int num1 = Convert.ToInt32 ( Console.ReadLine());;
+int num1 = Convert.ToInt32 ( Console.ReadLine());
 Console.WriteLine("Введите второе число");
-int num2 = Convert.ToInt32 ( Console.ReadLine());;
+int num2 = Convert.ToInt32 ( Console.ReadLine());
 if (num1 > num2) Console.WriteLine("Первое число большее, второе меньшее");
 if (num2 > num1) Console.WriteLine("Второе число большее, первое меньшее");
 if (num1 == num2) Console.WriteLine("Первое и второе число равны");
