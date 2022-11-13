6ebee1c0f1818620295b422794fa8243c0cad6bb
TatYana <4inenova@gmail.com>
2022-11-13 18:01:24 +0300

    add Homework 9 all

--- HW9/Program.cs
+++ HW9/Program.cs
@@ -2,14 +2,14 @@
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
-Console.Write ("Введите число N  ");
-int n = Convert.ToInt32(Console.ReadLine());
-string CountRec(int n)
-{
-    if (n==0) return "  ";
-       return  $"{n} "+ CountRec(n-1); 
-}
-Console.WriteLine(CountRec(n));
+// Console.Write ("Введите число N  ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// string CountRec(int n)
+// {
+//     if (n==0) return "  ";
+//        return  $"{n} "+ CountRec(n-1); 
+// }
+// Console.WriteLine(CountRec(n));
 
 // Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 // M = 1; N = 15 -> 120
@@ -28,14 +28,14 @@ Console.WriteLine(CountRec(n));
 // Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
-// Console.Write("Введите целое число m ");
-// int m = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Введите целое число n ");
-// int n = Convert.ToInt32(Console.ReadLine());
-// int FunAk (int m, int n)
-// {
-//     if (m == 0) return n + 1;
-//     else if (n == 0 && m > 0) return FunAk(m - 1, 1);
-//     else return (FunAk(m - 1, FunAk(m, n - 1)));
-// }
-// Console.WriteLine(FunAk(m,n));
+Console.Write("Введите целое число m ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое число n ");
+int n = Convert.ToInt32(Console.ReadLine());
+int FunAk (int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (n == 0 && m > 0) return FunAk(m - 1, 1);
+    else return (FunAk(m - 1, FunAk(m, n - 1)));
+}
+Console.WriteLine(FunAk(m,n));
